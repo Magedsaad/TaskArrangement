8ff6e5ddacfb61064f4fc3e1441aa10488506e6b
developing <magedabdelkhalik95@gmail.com>
2018-05-03 05:06:37 +0200

    addition

--- app/src/main/java/com/trying/developing/taskarrangement/adapter/MessagesAdapter.java
+++ app/src/main/java/com/trying/developing/taskarrangement/adapter/MessagesAdapter.java
@@ -1,8 +1,61 @@
 package com.trying.developing.taskarrangement.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.trying.developing.taskarrangement.R;
+import com.trying.developing.taskarrangement.model.Chat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by developing on 5/1/2018.
  */
 
-public class MessagesAdapter {
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MessagesHolder> {
+    Context mContext;
+    List<Chat> data=new ArrayList<>();
+
+    public MessagesAdapter(Context mContext, List<Chat> data) {
+        this.mContext = mContext;
+        this.data = data;
+    }
+
+    @Override
+    public MessagesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.chattinglistitem,parent,false);
+
+        return new  MessagesHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MessagesHolder holder, int position) {
+        Chat chat=data.get(position);
+        holder.messages.setText(chat.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MessagesHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.messagesTextView)
+        TextView messages;
+
+        public MessagesHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
 }
--- app/src/main/java/com/trying/developing/taskarrangement/ui/ChattingActivity.java
+++ app/src/main/java/com/trying/developing/taskarrangement/ui/ChattingActivity.java
@@ -3,6 +3,9 @@ package com.trying.developing.taskarrangement.ui;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,10 +14,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.trying.developing.taskarrangement.R;
+import com.trying.developing.taskarrangement.adapter.MessagesAdapter;
 import com.trying.developing.taskarrangement.model.Chat;
+import com.trying.developing.taskarrangement.model.Tasks;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,7 +38,10 @@ public class ChattingActivity extends AppCompatActivity {
     private DatabaseReference mChatDatabase;
     private FirebaseUser user;
     private FirebaseAuth mAuth;
-
+    private FirebaseDatabase FDB;
+    private List<Chat>chatList;
+    private MessagesAdapter messagesAdapter;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,38 +57,85 @@ public class ChattingActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        recyclerView=(RecyclerView) findViewById(R.id.chattingRecycler);
+        RecyclerView.LayoutManager manager=new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(),LinearLayoutManager.VERTICAL));
+
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
-        mChatDatabase = FirebaseDatabase.getInstance().getReference();
-
-        Toast.makeText(getApplicationContext(),friend_id,Toast.LENGTH_SHORT).show();
 
 
+        FDB=FirebaseDatabase.getInstance();
+
+        chatList=new ArrayList<>();
+        messagesAdapter=new MessagesAdapter(getApplicationContext(),chatList);
+
+        //Toast.makeText(getApplicationContext(),friend_id,Toast.LENGTH_SHORT).show();
+
+        showMessages();
 
     }
 
 
     public void send(View view){
 
-        String mCurrent_userid=user.getUid();
-        String mMessages=messagesarea.getText().toString();
+        final String mCurrent_userid=user.getUid();
+        final String mMessages=messagesarea.getText().toString();
 
         final Chat chats=new Chat(mMessages,friend_id);
-
-        mChatDatabase.child("chat").child(mCurrent_userid).push().setValue(chats).addOnCompleteListener(new OnCompleteListener<Void>() {
+        mChatDatabase = FirebaseDatabase.getInstance().getReference();
+        mChatDatabase.child("chat").child(mCurrent_userid).child(friend_id).push().setValue(chats).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
 
-                mChatDatabase.child("chat").child(friend_id).push().setValue(chats);
+                mChatDatabase.child("chat").child(friend_id).child(mCurrent_userid).push().setValue(chats);
 
-                Toast.makeText(getApplicationContext(),friend_id,Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(getApplicationContext(),friend_id,Toast.LENGTH_SHORT).show();
+                messagesarea.setText("");
+            }
+        });
+    }
+
+    public void showMessages(){
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        final String currentid=currentUser.getUid();
+        mChatDatabase=FDB.getReference("chat").child(currentid).child(friend_id);
+        mChatDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Chat data=dataSnapshot.getValue(Chat.class);
+                chatList.add(data);
+                //Toast.makeText(getApplicationContext(),data.getMessage(),Toast.LENGTH_SHORT).show();
+                messagesAdapter.notifyDataSetChanged();
+                recyclerView.setAdapter(messagesAdapter);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
 
 
-
-
     }
+
 }
